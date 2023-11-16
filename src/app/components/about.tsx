00914d059b2f1313106b857e5952cce8ac3227bc
LemonTheSour <m.m.downing@bigpond.com>
2023-11-16 18:52:18 +0800

    images added

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -1,7 +1,17 @@
 export default function About() {
   return (
-    <div>
-      <div>About</div>
+    <div className="text-center">
+      <div>Isobel Boddenham</div>
+      <div>
+        Veniam est do ullamco commodo occaecat. Tempor cillum ullamco velit laborum nulla 
+        ullamco ullamco quis et. Mollit ipsum elit nulla in. Minim cupidatat et non esse 
+        deserunt dolore labore dolore elit fugiat id magna. Aliqua ullamco laboris ut 
+        deserunt incididunt. Irure laborum aliqua laboris ex mollit culpa. Tempor consectetur 
+        occaecat quis et.
+      </div>
+      <div>
+        mobile: 0444 444 444 | contact jfjfjfjf
+      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 import About from "./components/about";
+import Image from "next/image";
+import Photo1 from "./assets/images/photo1.jpg"
+import Photo2 from "./assets/images/photo2.jpg"
+import Photo3 from "./assets/images/photo3.jpeg"
+import Photo4 from "./assets/images/photo4.jpg"
 
 export default function Home() {
   return (
     <div className="flex justify-center">
       <div className="pt-10">
         <About />
+        <Image src={Photo1} alt="Photo1" className="w-full"/>
+        <Image src={Photo2} alt="Photo2" className="w-full"/>
+        <Image src={Photo3} alt="Photo3" className="w-full"/>
+        <Image src={Photo4} alt="Photo4" className="w-full"/>
       </div>
     </div>
   );
