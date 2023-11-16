e9aa45e8c6c18313ba2b8eff1eeb035bd2db9915
Michael <m.m.downing@bigpond.com>
2023-11-16 17:25:15 +0800

    cleaned up dir

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import About from "./components/about";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div className="flex justify-center">
+      <div className="pt-10">
+        <About />
+      </div>
+    </div>
+  );
 }
