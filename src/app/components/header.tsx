4b8ebe26b37f9bd1848178bbac6c54621296772c
Michael <m.m.downing@bigpond.com>
2023-12-09 19:19:45 +0800

    basic styling changes

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,19 +3,21 @@ import FacebookIcon from "./facebook";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center bg-white p-2 h-16 sticky top-0 border-b-2 border-sunset">
-      <div>
-        <Link href="/">Isobel</Link>
-      </div>
-      <div className="space-x-8">
-        <Link href="/">Home</Link>
-        <Link href="/contact">Contact</Link>
-        <Link href="/faq">FAQ</Link>
-      </div>
-      <div>
-        <Link href="https://www.facebook.com/search/top?q=isobel%20bodenham%20marriage%20celebrant">
-          <FacebookIcon />
-        </Link>
+    <header className="flex items-center justify-center bg-white p-2 h-16 sticky top-0 border-b-2 border-sunset">
+      <div className="flex justify-between items-center justify-center w-4/6">
+        <div>
+          <Link href="/">Isobel</Link>
+        </div>
+        <div className="space-x-8">
+          <Link href="/">Home</Link>
+          <Link href="/contact">Contact</Link>
+          <Link href="/faq">FAQ</Link>
+        </div>
+        <div>
+          <Link href="https://www.facebook.com/search/top?q=isobel%20bodenham%20marriage%20celebrant">
+            <FacebookIcon />
+          </Link>
+        </div>
       </div>
     </header>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,11 @@ export default function Home() {
   return (
     <div>
       <div>
-        <div className="bg-photo1 bg-no-repeat bg-cover bg-center bg-fixed w-full h-screen z-0"></div>
+        <div className="bg-photo1 bg-no-repeat bg-cover bg-center bg-fixed w-full h-screen z-0">
+          <div className="w-full h-screen flex pt-20 content-center justify-end align-center">
+            <div className="bg-white w-5/6 border-sunset border-4 h-3/4 rounded-s-full"></div>
+          </div>
+        </div>
         <div className="bg-sunset h-64 w-full z-10"></div>
       </div>
       <div>
