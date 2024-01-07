c5a24bd3ea42980f63bfb70c22ec224bc91a0385
Michael <m.m.downing@bigpond.com>
2024-01-07 20:09:19 +0800

    second home page implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import { Italianno } from "next/font/google";
 import Link from "next/link";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Home",
@@ -14,36 +15,56 @@ const italianno = Italianno({
 export default function Home() {
   return (
     <div>
-      <div>
-        <div className="flex justify-center bg-photo1 bg-no-repeat bg-cover bg-center bg-fixed w-full h-screen z-0">
-          <div className="flex flex-col items-center">
-            <div
-              className={`text-9xl text-white text-center mt-40 ${italianno.className}`}
-            >
-              <div className="block">Isobel Boddenham</div>
-              <div>Marriage Celebrant</div>
-            </div>
-            <div className="text-center text-white pt-12 pb-12 text-2xl">
-              tel: 0444 444 444 | email: example@bigpond.com
-            </div>
-            <Link
-              className="flex bg-sunset w-64 h-24 text-2xl items-center justify-center rounded text-white"
-              href={"/contact"}
-            >
-              Contact
-            </Link>
+      {/* First Scroll Page */}
+      <div className="flex justify-center bg-photo1 bg-no-repeat bg-cover bg-center bg-fixed w-full h-screen z-0">
+        <div className="flex w-4/6 flex-col items-center">
+          <div
+            className={`text-9xl text-white text-center mt-40 ${italianno.className}`}
+          >
+            <div className="block">Isobel Boddenham</div>
+            <div>Marriage Celebrant</div>
+          </div>
+          <div className="text-center text-white pt-12 pb-8 text-2xl">
+            <div>mobile: 0444 444 444</div>
+            <div>email: example@bigpond.com.au</div>
+          </div>
+
+          <Link
+            className="flex bg-sunset w-64 h-20 text-2xl items-center justify-center rounded text-white"
+            href={"/contact"}
+          >
+            Contact
+          </Link>
+        </div>
+      </div>
+      <div className="bg-sunset h-32 w-full z-10"></div>
+
+      {/* Second Scroll Page */}
+      <div className="flex justify-center items-center bg-photo2 bg-no-repeat bg-cover bg-center bg-fixed w-full h-screen z-1">
+        <div className="flex w-4/6 pl-20 pb-60">
+          <div className="w-80 h-96 relative">
+            <Image
+              src={"/images/photo6.jpg"}
+              alt={"Isobel"}
+              layout="fill"
+              className="rounded rotate-3 drop-shadow-2xl"
+            />
+          </div>
+          <div className="pl-20 w-1/2 text-white text-xl">
+            Amet duis sint adipisicing nostrud adipisicing labore mollit tempor
+            sunt reprehenderit voluptate. Proident ut veniam consectetur
+            deserunt do commodo culpa ex cupidatat Lorem id. Sunt excepteur
+            consectetur magna sit eu magna Lorem fugiat. Aliquip fugiat aute
+            magna eu et excepteur anim laboris commodo ut officia amet
+            incididunt. Cillum Lorem proident minim ut et. Lorem id. Sunt
+            excepteur consectetur magna sit eu magna Lorem fugiat. Aliquip
+            fugiat aute magna eu et excepteur anim laboris commodo ut officia
+            amet incididunt. Cillum Lorem proident minim ut et.
           </div>
         </div>
-        <div className="bg-sunset h-64 w-full z-10"></div>
       </div>
-      <div>
-        <div className="bg-photo2 bg-no-repeat bg-cover bg-center bg-fixed w-full h-screen z-1"></div>
-        <div className="bg-sunset h-64 w-full z-10"></div>
-      </div>
-      <div>
-        <div className="bg-photo3 bg-no-repeat bg-cover bg-center bg-fixed w-full h-screen z-2"></div>
-        <div className="bg-sunset h-64 w-full z-10"></div>
-      </div>
+      <div className="bg-sunset h-32 w-full z-10"></div>
+      {/* Third Scroll Page */}
       <div>
         <div className="bg-photo4 bg-no-repeat bg-cover bg-center bg-fixed w-full h-screen z-3"></div>
       </div>
